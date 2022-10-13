7a327d0f124e4118669e518e16041d07dc6dedb5
Rayeh1 <rayeh16@gmail.com>
2022-10-13 16:40:45 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: and that piece of art is useful
+ *
+ *Return: 1 if result is as expected else 0
+ */
+int main(void)
+{
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
