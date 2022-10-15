54938cdd30844cad566b22e5623a1ff05610bc41
Rayeh1 <rayeh16@gmail.com>
2022-10-14 18:33:08 -0700

    com

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,7 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- *main -  prints the alphabet in lowercase,
- *Challenge: You can only use the putchar twice
+ *main -  prints the alphabet in lowercase
  *Return: 0 if result is as expected otherwise !0
  */
 
@@ -13,10 +9,10 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++);
+	for (c = 'a'; c <= 'z'; c++) 
 	{
 		putchar(c);
-	}							}
+	}
 	putchar('\n');
 	return (0);
 }
