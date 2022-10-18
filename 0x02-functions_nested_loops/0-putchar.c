18b7f78f22fdb98f518055c8d6fa9aa24914fd43
Rayeh1 <rayeh16@gmail.com>
2022-10-18 09:35:27 -0700

    com

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.c"
 
 /**
  * main - entry point
@@ -7,6 +7,11 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char c;
+	for(c='putchar')
+	{	
+	putchar(c);
+	}
+	putchar('\n')
 	return (0);
 }
