846e937757e8481fa77df59395c8c6aa55df5ca0
Rayeh1 <rayeh16@gmail.com>
2022-10-19 09:02:32 -0700

    com

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_sign - print sign of a number
+ *@n: operand to print sign
+ *Return: Return 1 if positive, else if return 0 if zero, else -0
+ */
+int print_sign(int n)
+{
+	{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	}
+	{
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+	{
+	else (n < 0)
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
+}
