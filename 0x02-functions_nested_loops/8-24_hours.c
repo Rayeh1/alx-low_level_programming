c4f7646521e02381041bf5bc9572baa1ac24e9f6
Rayeh1 <rayeh16@gmail.com>
2022-10-19 14:56:16 -0700

    com

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,11 @@ void jack_bauer(void)
 	for (x = 0; x <= 23; x++)
 		for (y = 0; y <= 59; y++)
 		{
-		_putchar(x + '0');
+		_putchar((x / 10) + '0');
+		_putchar((x % 10) + '0');
 		_putchar(':');
-		_putchar(y + '0');
+		_putchar((y / 10) + '0');
+		_putchar((y % 10) + '0');
 		}
 	_putchar('\n');
 }
