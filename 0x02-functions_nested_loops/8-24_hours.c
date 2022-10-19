e977a5407afaf205a6cb00064d68a4c8281a8d55
Rayeh1 <rayeh16@gmail.com>
2022-10-19 15:09:03 -0700

    com

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * jack_bauer: theme song
- * Return: Always 0 (success)
+ *jack_bauer - function that prints every minute
+ *@void - function that prints every minute of the day of Jack Bauer
+ *Return: Always 0 (success)
  */
 void jack_bauer(void)
 {
