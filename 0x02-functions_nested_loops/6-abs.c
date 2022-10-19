0c18b1cba25854086fc87abdff1a6aa9254a66c0
Rayeh1 <rayeh16@gmail.com>
2022-10-19 10:05:17 -0700

    com

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,17 @@
 
 /**
  *int _abs -  computes the absolute value of an integer
- *Return: void
+ *Return: absolute number of integer
  */
 int _abs(int n)
 {
 	if (n >= 0)
+	{
 		_putchar(n);
-	else (n < 0)
+	}
+	else
+	{
 		_putchar(-n);
+	}
+	_putchar('\n');
 }	
