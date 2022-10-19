6ab55fcde71b9e1dbb79d152c5db4c450301d951
Rayeh1 <rayeh16@gmail.com>
2022-10-19 12:33:39 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_last_digit - print last digit of a number 
+ *@n: operand to rep integer
+ *Return: Return the last digit
+ */
+int print_last_digit(int n)
+{
+	int result;
+
+	if (result = n % 10)
+	return (result);
+}
