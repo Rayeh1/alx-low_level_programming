d323e1018746ba2269bd724996be4e05a22167d3
Rayeh1 <rayeh16@gmail.com>
2022-10-20 08:22:47 -0700

    2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,14 +15,20 @@ int largest_number(int a, int b, int c)
 	if (a > b && b > c) || (a > c && c > b)
 	{
 		largest = a;
+		printf("largest =");
+		putchar(a);
 	}
 	else if (b > a && a > c) || (b > c && c > a)
 	{
 		largest = b;
+		printf("largest =");
+		putchar(b);
 	}
 	else
 	{
 		largest = c;
+		printf("largest =");
+		putchar(c);
 	}
 
 	return (largest);
