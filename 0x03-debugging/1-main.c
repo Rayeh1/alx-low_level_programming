f6ad59f07c779dd8c32d268b75f198989f2dd955
Rayeh1 <rayeh16@gmail.com>
2022-10-20 07:06:31 -0700

    com

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,12 @@ int main(void)
 
 	i = 0;
 
-	if (i < 10)
-	{
-		putchar(i);
-	}
+	/* (i < 10) */
+	/*{ */
+	/*	putchar(i); */
+	/*/} */
 
-	printf("Infinite loop avoided! \\\\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
