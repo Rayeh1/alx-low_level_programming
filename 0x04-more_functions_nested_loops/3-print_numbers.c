f0cb1ec48dcfb13bf1525c59c06566e7811201ea
Rayeh1 <rayeh16@gmail.com>
2022-10-20 17:46:31 -0700

    com

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_numbers - print numbers
+ *_putchar - to print numbers
+ *Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	char c;
+	for (c >= '0'; c <= '9'; c++)
+		_putchar(c);
+		_putchar('\n');
+}
