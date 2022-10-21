bfbcb860a482340378b3add32fb0c278ecf77056
Rayeh1 <rayeh16@gmail.com>
2022-10-20 18:34:06 -0700

    com

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,6 +3,7 @@
 /**
  *print_most_numbers - prints numbbers from 0 to 9
  *_putchar - to print
+ *@c: operand to print
  *Return: Always 0 (always)
  */
 void print_most_numbers(void)
@@ -11,7 +12,7 @@ void print_most_numbers(void)
 
 	for (c = '0'; c >= '9'; c++)
 	{
-		if ((c != '2') && (c != '4'))
+		if (c != '2' && c != '4')
 			_putchar(c);
 	}
 	_putchar('\n');
