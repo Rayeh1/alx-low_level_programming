dafaeb66481e62e1b28f010457eecb535d8c193e
Rayeh1 <rayeh16@gmail.com>
2022-10-24 17:31:06 -0700

    com

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - function
+ * @s: variable
+ * @c:length
+ * Return: always 0 (success)
+ */
+int _strlen(char *s)
+{
+	int len;
+	len = _strlen(s);
+	return (len);
+}
