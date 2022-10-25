5112008c31361e28ff75c196f8c08d12010a0afa
Rayeh1 <rayeh16@gmail.com>
2022-10-24 20:04:15 -0700

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,15 @@
 
 /**
  * _puts - function
- *str: string 
+ *@str: string
+ *@c: index
  * return: always 0 (success)
  */
 void _puts(char *str)
 {
 	int c;
+
+	while (str[c] != 0)
 	{
 	_putchar(str[c]);
 	}
