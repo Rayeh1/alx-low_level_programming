3a7c4cc145140e1fe7ca1bef9b94fd3fcb023cd3
Rayeh1 <rayeh16@gmail.com>
2022-10-26 14:18:05 -0700

    1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strcat - function
+ * @dest: string 2
+ * @src: string 1
+ * Return: dest
+ */
+
+
 char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
