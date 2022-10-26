fb26e592ff89594bb86232cb1fe9acc0bc0e5947
Rayeh1 <rayeh16@gmail.com>
2022-10-26 15:19:24 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - function
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 (if same)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+	int b;
+
+	s1[a];
+	s2[b];
+	if (s1[a] > s2[b])
+	{
+		return (a - b);
+	}
+	else if (s2[b] > s1[a])
+	{
+		return (b - a);
+	}
+	else
+	{
+		return(0);
+	}
+}
