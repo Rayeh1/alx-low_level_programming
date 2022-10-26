1b0b7f80aa58c5be92c25e27f2be2f921f22c2f6
Rayeh1 <rayeh16@gmail.com>
2022-10-26 14:27:23 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function
+ * @n; integer
+ * @dest: string 2
+ * @src: string 1
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	_strncat(dest, src, n);
+	return (dest);
+}
