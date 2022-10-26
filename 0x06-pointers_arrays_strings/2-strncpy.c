efa2e21426ebd842f14162e04bdc9831e2bcf5b8
Rayeh1 <rayeh16@gmail.com>
2022-10-26 14:47:00 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * @n: integer
+ * @dest: integer 1
+ * @src:integer 2
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	_strncpy(dest, src, n);
+	return (dest);
+}
