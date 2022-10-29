fdb9145acaa707c902df11bf7d16ea622b7d057a
Rayeh1 <rayeh16@gmail.com>
2022-10-28 20:30:14 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,21 +8,10 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int *x;
-	int z;
 	int b;
 
-	for (b = 0; b < n; b++)
-	{
+	for (b = n - 1; b >= n; b--)
+		{
 		a[b];
-		x[z] = a[b];
-	}
-
-	i = n - 1;
-	for (z = i; z >= 0; z--)
-	{
-		x[z];
-		a[b] = x[z];
-	}
+		}
 }
