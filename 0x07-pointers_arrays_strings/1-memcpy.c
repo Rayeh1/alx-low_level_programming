52eca8ad10dea57b75d26161bc4faa352f5e5531
Rayeh1 <rayeh16@gmail.com>
2022-11-01 14:43:06 -0700

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _memcpy - funtion
+ * @dest: str1
+ * @src: str2
+ * @n: integ
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	n = &src;
+	dest = &n;
+
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define HEADER_H
 
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 
