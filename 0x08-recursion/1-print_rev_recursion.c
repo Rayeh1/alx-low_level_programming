6d50e2ecb8faf98e9a1c4dafdeebfc4feead585a
Rayeh1 <rayeh16@gmail.com>
2022-11-02 16:40:03 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion
+ * @s: string 1
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s++);
+	else
+		return;
+	_putchar(*s);
+}
+
+int main(void)
+{
+	_print_rev_recursion("\nColton Walker");
+	return (0);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void _print_rev_recursion(char *s);
+int _putchar(char);
+
+
+
+
+
+
+
+
+
+#endif
