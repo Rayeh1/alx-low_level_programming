d3bd3825ce885d0a3b5d7fe944e127d789a337db
Rayeh1 <rayeh16@gmail.com>
2022-11-02 17:27:36 -0700

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - function
+ * @n: integer
+ * Return: void
+ */
+int factorial(int n)
+{
+	if (n >= 0)
+	{
+		return n * factorial(n - 1);
+	}
+	else
+		return (-1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,7 +4,7 @@
 void _print_rev_recursion(char *s);
 int _putchar(char);
 void _puts_recursion(char *s);
-
+int factorial(int n);
 
 
 
