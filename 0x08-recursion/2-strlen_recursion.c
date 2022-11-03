80549eb659a54221b0f54819a7665ee5a0ce069b
Rayeh1 <rayeh16@gmail.com>
2022-11-02 17:53:39 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+int x;
+
+/**
+ * _strlen_recursion - function
+ * @s: string
+ * Return: s
+ */
+int _strlen_recursion(char *s)
+{
+	x = strlen(s);
+	return (x);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,7 @@ void _print_rev_recursion(char *s);
 int _putchar(char);
 void _puts_recursion(char *s);
 int factorial(int n);
-
+int _strlen_recursion(char *s);
 
 
 
