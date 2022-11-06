2da8835f5481a6fc98aa0d069ad15b0f123938fb
Rayeh1 <rayeh16@gmail.com>
2022-11-06 10:19:21 -0800

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - print number
+ * @argc: numb
+ * @argv: args
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
