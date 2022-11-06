fc62c45bf814bda001dc4c20627f00a102d0b963
Rayeh1 <rayeh16@gmail.com>
2022-11-06 10:59:49 -0800

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,10 +13,14 @@ int main(int argc, char *argv[])
 	int y;
 	int result;
 
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
 	result = x * y;
-	if (argc < 2)
+
+	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
+		return (1);
 	}
 	else
 	{
