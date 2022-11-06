c8eca84e55a8e75bd6276b8b8ade20b98ee0feaa
Rayeh1 <rayeh16@gmail.com>
2022-11-06 09:40:44 -0800

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Program that prints name - function
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_h
+
+int _putchar(char);
+int checknum(char);
+
+
+#endif
