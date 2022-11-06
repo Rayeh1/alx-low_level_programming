2e23004bf24f198b7f6561391d142905cc0cf9e5
Rayeh1 <rayeh16@gmail.com>
2022-11-06 10:32:26 -0800

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - function
+ *@argc: number
+ *@argv: args
+ *Return: void
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
