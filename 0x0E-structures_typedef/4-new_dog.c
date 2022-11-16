23c88dd8341f895ed05e06815dc7d44ea78cd34a
Rayeh1 <rayeh16@gmail.com>
2022-11-15 19:08:37 -0800

    com

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,14 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *  * new_dog - creates new dog
- *   * @name: name of dog
- *    * @age: age of dog
- *     * @owner: owner of dog
- *      * Return: a dog object
- *       */
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: a dog object
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, len1, len2;
@@ -17,20 +18,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	len1 = 0;
 	for (i = 0; name[i] != '\0'; i++)
 		len1++;
-		len2 = 0;
-		for (i = 0; owner[i] != '\0'; i++)
-			len2++;
-		ndog = malloc(sizeof(dog_t));
-		if (ndog == NULL)
-			return (NULL);
-		a = malloc((len1 + 1) * sizeof(char));
-		if (a == NULL)
+	len2 = 0;
+	for (i = 0; owner[i] != '\0'; i++)
+		len2++;
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	a = malloc((len1 + 1) * sizeof(char));
+	if (a == NULL)
 	{
 		free(ndog);
 		return (NULL);
 	}
 	for (i = 0; i < len1; i++)
-	a[i] = name[i];
+		a[i] = name[i];
 	a[i] = '\0';
 	ndog->name = a;
 	ndog->age = age;
