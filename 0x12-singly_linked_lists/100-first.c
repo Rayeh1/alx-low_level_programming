77044908916ca8b09af93869318a8746c76f48f3
Rayeh1 <rayeh16@gmail.com>
2022-11-25 02:34:58 -0800

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- *
  * bmain - function executed before main
  *
  * Return: no return.
- *
  */
 void __attribute__((constructor)) bmain(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
