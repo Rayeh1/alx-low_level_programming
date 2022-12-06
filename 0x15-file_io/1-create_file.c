3326618d85a81ee52940643ce8b140b802547704
Rayeh1 <rayeh16@gmail.com>
2022-12-06 13:47:51 -0800

    2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file and adds permission to it
  * @filename: The name of the file to be created
- * @text_content: content to be placed in the created file 
+ * @text_content: content to be placed in the created file
  * Return: 1 if successfull and -1 on err.
  */
 int create_file(const char *filename, char *text_content)
