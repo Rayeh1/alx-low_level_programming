cb11ecbeb75ebeb4d3155deae9e15cf3a909ac5f
Rayeh1 <rayeh16@gmail.com>
2022-12-06 13:35:14 -0800

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	chk = read(fild, buffer, letters);
 	chk = write(STDOUT_FILENO, buffer, chk);
-		if (chk < 0)
+	if (chk < 0)
 	return (0);
 
 	close(fild);
